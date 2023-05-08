43453ec39bb016049159e477f7b56711f5a0ce75
Your Name <you@example.com>
2023-05-08 13:19:33 +0000

    ok

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable vpc_cidr_block {
+    default = "10.0.0.0/16"
+}
+variable subnet_cidr_block {
+    default = "10.0.10.0/24"
+}
+variable avail_zone {
+    default = "eu-west-3a"
+}
+variable env_prefix {
+    default = "dev"
+}
+variable my_ip {
+    default = "212.124.154.110/32"
+}
+variable jenkins_ip {
+    default = "139.59.140.177/32"
+}
+variable instance_type {
+    default = "t2.micro"
+}
+variable region {
+    default = "eu-west-3"
+}
